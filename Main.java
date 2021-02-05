4e2b5ac61dd6776971b6e4e4619642657c04c4e9
SaranyaUthayasooriyan <sarancse.saran@gmail.com>
2021-02-05 19:17:57 +0530

    Updated Main java

--- Main.java
+++ Main.java
@@ -6,7 +6,13 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		System.out.println("My first Application");
-
+		int i=2;
+		if(i==0)
+		{
+			System.out.println("The integer value is 0");
+		}else{
+			System.out.println("The value is not 0");
+		}
 	}
 
 }
